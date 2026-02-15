d0e37d5373450a76c81273ce777355a01bec14a9
Warren Parad <gpg@warrenparad.net>
2026-02-15 21:20:01 +0100

    Add object handling in antiabuse hashing.

--- src/jwtManager.js
+++ src/jwtManager.js
@@ -61,7 +61,8 @@ class JwtManager {
 
   async calculateAntiAbuseHash(props) {
     const timestamp = Date.now();
-    const valueString = Object.values(props).filter(v => v).join('|');
+    const valueString = Object.values(props).filter(v => v)
+    .map(v => v && typeof v === 'object' && !Array.isArray(v) ? JSON.stringify(v) : v).join('|');
 
     let fineTuner = 0;
     while (++fineTuner) {
